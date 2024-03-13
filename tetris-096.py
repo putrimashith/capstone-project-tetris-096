0dfd3a4d8293ac6082afa9549fbda2216546f088
putrimashith <154588265+putrimashith@users.noreply.github.com>
2024-03-13 10:30:14 +0700

    Update tetris-096.py

--- tetris-096.py
+++ tetris-096.py
@@ -4,30 +4,34 @@ import altair as alt
 import streamlit as st
 
 st.set_page_config(
-    page_title = 'Pengaruh Penanaman Modal dalam Mengurangi Tingkat Pengangguran Terbuka di Jawa Barat'
+    page_title = 'Pengaruh Penanaman Modal dalam Mengurangi Tingkat Pengangguran Terbuka di Jawa Barat Tahun 2012-2021'
     ,layout='wide'
 )
 
-st.title('Pengaruh Penanaman Modal dalam Mengurangi Tingkat Pengangguran Terbuka di Jawa Barat')
+st.title('Pengaruh Penanaman Modal dalam Mengurangi Tingkat Pengangguran Terbuka di Jawa Barat Tahun 2012-2021')
 st.caption('oleh: Mashithoh Putri NI')
 
 # Tabbing
-tab1, tab2, tab3 = st.tabs(['**Latar Belakang dan Hipotesa**', '**Data Visualisasi**', '**Kesimpulan dan Saran**'])
+tab1, tab2, tab3, tab4 = st.tabs(['**Latar Belakang dan Pertanyaan Analisis**', '**Data Visualisasi**', '**Kesimpulan dan Saran**', '**Referensi**'])
 
 with tab1:
     st.header('Latar Belakang')
-    st.write("""
+    st.write("""<div style="text-align: justify">
 Indonesia sebagai negara berkembang yang memiliki letak strategis menjadi salah satu alasan mengapa para investor ingin menanamkan modal untuk berinvestasi.
 Selain itu, adanya Sumber Daya Manusia yang melimpah dianggap sebagai sebuah nilai tambah dalam menjalankan proyek investasinya.
-             
-Apa itu penanaman modal? berdasarkan **Pasal 1 angka 1 UU 25/2007**, penanaman modal adalah segala bentuk kegiatan menanam modal,
+Pada tahun 2021, tercatat jumlah penduduk Indonesia telah mencapai <strong>273.8 juta jiwa</strong>
+(*<em><strong>sumber:</strong> World Bank, United States Census Bureau*</em>).
+
+<br>Apa itu penanaman modal? berdasarkan <strong>Pasal 1 angka 1 UU 25/2007</strong>, penanaman modal adalah segala bentuk kegiatan menanam modal,
 baik oleh penanam modal dalam negeri maupun penanam modal asing untuk melakukan usaha di wilayah negara Republik Indonesia.
              
-Disisi lain, Jawa Barat sebagai provinsi dengan jumlah penduduk terbanyak di Indonesia, banyak dilirik oleh para investor untuk berinvestasi.
+Disisi lain, Jawa Barat sebagai provinsi dengan jumlah penduduk terbanyak di Indonesia, yaitu **48.782 jiwa** (***sumber:** BPS Sulawesi Utara, 2021*)
+banyak dilirik oleh para investor untuk berinvestasi.
 Namun, apakah hal ini memberikan efek yang signifikan dalam menurunkan jumlah pengangguran terbuka di provinsi tersebut?
-""")
+</div>
+""", unsafe_allow_html=True)
     
-    st.header('Hipotesa')
+    st.header('Pertanyaan Analisis')
     st.markdown("""
     1. Bagaimana sebaran data penanaman modal di Provinsi Jawa Barat berdasarkan Kota/Kabupaten?
     2. Bagaimana sebaran data penanaman modal di Provinsi Jawa Barat berdasarkan Sektor Usaha?
@@ -74,7 +78,7 @@ with tab2:
 # Function to format large numbers
     def format_big_number(num):
         if num >= 1e9:
-            return f"{num / 1e9:.2f} Mld"
+            return f"{num / 1e9:.2f} Miliar"
         elif num >= 1e6:
             return f"{num / 1e6:.2f} Mio"
         elif num >= 1e3:
@@ -179,17 +183,21 @@ with tab2:
     st.markdown('---')
     # st.write(df1)
 
-    # Buat pilihan tahun menggunakan list unik dari kolom tahun
-    tahun_pilihan = sorted(df1['tahun'].dt.year.unique())
+    # # Buat pilihan tahun menggunakan list unik dari kolom tahun
+    # tahun_pilihan = sorted(df1['tahun'].dt.year.unique())
 
-    # Buat selectbox untuk memilih tahun
-    tahun_terpilih = st.selectbox("Pilih Tahun", tahun_pilihan)
+    # # Buat selectbox untuk memilih tahun
+    # tahun_terpilih = st.selectbox("Pilih Tahun", tahun_pilihan)
+
+    # # Tambahkan subheader
+    # st.subheader("Sebaran Data Jumlah Proyek Penanamana Modal per Kab/Kota di Jawa Barat Tahun {}".format(tahun_terpilih))
+
+    # # Filter data berdasarkan tahun yang dipilih
+    # df_terfilter = df1[df1['tahun'] == tahun_terpilih]
 
     # Tambahkan subheader
-    st.subheader("Sebaran Data Jumlah Proyek Penanamana Modal per Kab/Kota di Jawa Barat Tahun {}".format(tahun_terpilih))
-
-    # Filter data berdasarkan tahun yang dipilih
-    df_terfilter = df1[df1['tahun'] == tahun_terpilih]
+    st.subheader("Sebaran Data Jumlah Proyek Penanamana Modal per Kab/Kota di Jawa Barat Tahun 2012-2021")
+    st.markdown("")
 
     # Membuat bar chart interaktif
     bar_chart = alt.Chart(df1).transform_fold(
@@ -214,17 +222,21 @@ with tab2:
     df2['tahun'] = pd.to_datetime(df2['tahun'], format='%Y')
     # st.write(df2)
 
-    # Buat pilihan tahun menggunakan list unik dari kolom tahun
-    tahun_pilihan = sorted(df2['tahun'].dt.year.unique())
+    # # Buat pilihan tahun menggunakan list unik dari kolom tahun
+    # tahun_pilihan = sorted(df2['tahun'].dt.year.unique())
 
-    # Buat selectbox untuk memilih tahun
-    tahun_terpilih = st.selectbox("Tahun", tahun_pilihan)
+    # # Buat selectbox untuk memilih tahun
+    # tahun_terpilih = st.selectbox("Tahun", tahun_pilihan)
+
+    # # Tambahkan subheader
+    # st.subheader("Sebaran Data Jumlah Proyek Penanaman Modal per Sektor Usaha di Jawa Barat Tahun {}".format(tahun_terpilih))
+
+    # # Filter data berdasarkan tahun yang dipilih
+    # df_terfilter = df2[df2['tahun'] == tahun_terpilih]
 
     # Tambahkan subheader
-    st.subheader("Sebaran Data Jumlah Proyek Penanaman Modal per Sektor Usaha di Jawa Barat Tahun {}".format(tahun_terpilih))
-
-    # Filter data berdasarkan tahun yang dipilih
-    df_terfilter = df2[df2['tahun'] == tahun_terpilih]
+    st.subheader("Sebaran Data Jumlah Proyek Penanaman Modal per Sektor Usaha di Jawa Barat Tahun 2012-2021")
+    st.markdown("")
 
     # Membuat bar chart interaktif
     bar_chart = alt.Chart(df2).transform_fold(
@@ -271,11 +283,11 @@ with tab2:
     filtered_df = dataframes[variabel_selected]
 
     # Membuat grafik garis interaktif dengan Altair
-    grafik = alt.Chart(filtered_df, width=700, height=400).mark_line(point={"size": 100}).encode(
+    grafik = alt.Chart(filtered_df, width=200, height=400).mark_line(point={"size": 100}).encode(
         x='tahun:O',
         y=alt.Y('value:Q', title=variabel_selected),
         color='variable:N',
-        tooltip=['tahun:T', 'value:Q', 'variable:N']
+        tooltip=['tahun:O', 'value:Q', 'variable:N']
     ).transform_fold(
         fold=list(filtered_df.columns[1:]),
         as_=['variable', 'value']
@@ -286,10 +298,21 @@ with tab2:
 
     st.markdown('---')
     st.subheader('Korelasi Penanaman Modal dengan Tingkat Pengangguran Terbuka di Provinsi Jawa Barat')
-    st.write("""**Tingkat pengangguran terbuka** adalah persentase jumlah pengangguran terhadap jumlah angkatan kerja.
-             Angkatan Kerja adalah penduduk usia kerja (15 tahun ke atas) yang bekerja atau punya pekerjaan,
-             namun sementara tidak bekerja, dan penggangguran.
-             """)
+    st.write("""
+             <div style="text-align: justify">
+             <p><strong>TPT (Tingkat Pengangguran Terbuka)</strong> adalah persentase jumlah pengangguran terhadap jumlah angkatan kerja.
+             Angkatan Kerja adalah penduduk usia kerja (15 tahun ke atas) yang bekerja atau punya pekerjaan namun sementara tidak bekerja,
+             dan penggangguran.
+             Pengangguran yaitu: (1) penduduk yang aktif mencari pekerjaan, (2) penduduk yang sedang mempersiapkan usaha/pekerjaan baru,
+             (3) penduduk yang tidak mencari pekerjaan karena merasa tidak mungkin mendapat pekerjaan,
+             (4) kelompok penduduk yang tidak aktif mencari pekerjaan dengan alasan sudah mempunyai pekerjaan tetapi belum mulai bekerja.
+             (<em><strong>Sumber:</strong></em> <a href="https://bappeda.agamkab.go.id/Pojok/detail/46">https://bappeda.agamkab.go.id/Pojok/detail/46</a>)</p>
+             </div>
+             """, unsafe_allow_html=True)
+    st.markdown('')
+    st.markdown('')
+    
+
     df3 = pd.read_csv('https://drive.google.com/uc?export=download&id=130L_5cIG6l7n8mTLXxGJDPhYzsprS6bi')
     # st.write(df3)
 
@@ -314,26 +337,59 @@ with tab2:
         height=800
     )
     scatter_plot + linear_regression
-
     # Menghitung korelasi antara variabel 'tingkat_pengangguran_terbuka' dan 'tingkat_penyerapan_tk'
+
     correlation = df3['tingkat_penyerapan_tk'].corr(df3['tingkat_pengangguran_terbuka'])
-    st.write("Nilai korelasi:", correlation)
+
+    # Menampilkan nilai korelasi dengan warna
+    st.write("Nilai korelasi: <span style='color:blue'>{:.2f}</span>".format(correlation), unsafe_allow_html=True)
+
+    # Menampilkan info tambahan
+    st.info("""
+            Ketika tingkat penyerapan tenaga kerja di Jawa Barat naik sebesar 1%,
+            maka tingkat pengangguran terbuka di provinsi tersebut turun sebesar 0.3%.
+
+            Meskipun nilai korelasinya tidak besar, namun adanya penyerapan tenaga kerja dari proyek penanaman modal baik asing maupun domestik,
+            ternyata memiliki pengaruh dalam mengurangi jumlah pengangguran terbuka di Provinsi Jawa Barat.
+            """)
 
 with tab3:
     st.header('Kesimpulan')
     st.markdown("""
-    1. proyek penanaman modal di Provinsi Jawa Barat belum tersebar rata, ada kabupaten/kota yang kurang dilirik oleh para investor.
-    2. Berdasarkan sektor usaha, sektor perdagangan dan reparasi mendapat penanaman modal yang paling banyak, baik dari investor asing maupun domestik.
-       Sementara sektor lainnya cenderung normal.
-    3. Trend jumlah proyek baik asing maupun domestik meningkat dari tahun 2012-2021.
-       Disisi lain, trend penyerap  an tenaga kerja melalui proyek-proyek tersebut bererak fluktuatif.
-    4. Adanya proyek penanaman modal di Provinsi Jawa Barat sedikit mengurangi jumlah pengangguran di daerah tersebut.
-""")
+    1. **Tidak meratanya penanaman modal di Provinsi Jawa barat:** Dalam 10 tahun terakhir (2012-2021), **hampir 35%** dari total proyek penanaman modal
+        berada di **Kabupaten Bekasi**, sementara 65% sisanya tersebar di 26 kota dan kabupaten pada provinsi tersebut.
+    2. **Dominasi pada sektor usaha tertentu:** Dari data terlampir, **22.2%** dari total proyek penanaman modal ditanam pada Sektor Perdagangan dan Reparasi,
+    **13.7%** pada Sektor Industri Logam, Mesin, dan Elektronika. Sementara **64.1%** sisanya terbagi dalam 22 sektor lainnya.
+    3. **Trend Penanaman Modal dan Penyerapan Tenaga Kerja:** Trend penanaman modal yang meningkat dari tahun 2012-2021 menunjukkan kepercayaan investor
+    terhadap potensi ekonomi Jawa Barat. Namun, fluktuasi dalam penyerapan tenaga kerja mungkin menunjukkan masalah lain yang perlu dianalisis lebih lanjut.
+    4. **Dampak Penanaman Modal terhadap Pengangguran:** Adanya proyek penanaman modal di Jawa Barat telah sedikit membantu mengurangi jumlah pengangguran terbuka di daerah tersebut.
+    Namun, efek ini belum merata di seluruh kabupaten atau kota dan perlu tinjauan lebih lanjut untuk mengetahui sebab dari masalah tersebut.
+    """)
     
     st.header('Saran')
     st.markdown("""
-    Bagi para pemangku kepentingan, mungkin bisa melirik kabupaten/kota atau sektor usaha yang belum banyak mendapatkan penanaman modal.
+    <div style="text-align: justify">
+    Bagi para pemangku kepentingan, mungkin bisa melirik kabupaten/kota yang belum banyak mendapatkan penanaman modal.
+    Mengingat jumlah pengangguran terbuka di Provinsi Jawa Barat tersebar di 27 kota dan kabupaten, tidak hanya berpusat pada satu titik area.
+                
+    <br> <strong>Kabupaten Pangandaran<strong/> misalnya, dalam 10 tahun terakhir hanya memiliki <strong>78</strong> dari total <strong>125 ribu</strong>
+    proyek. Padahal, jika dilihat lebih seksama, daerah tersebut memiliki banyak potensi perekonomian yang bisa membantu menurunkan tingkat pengangguran terbuka, seperti potensi laut dan perikanan,
+    potensi wisata bahari dan sejarah, serta potensi kebudayaannya.
     
-    Mengingat jumlah pengangguran terbuka di Provinsi Jawa Barat tidak hanya terpusat pada beberapa kabupaten/kota,
-    dan tentunya setiap orang yang belum bekerja (sedang mencari pekerjaan) memiliki kemampuan berbeda di setiap sektor usahanya.
+    Namun, perlu digaris-bawahi juga, untuk meningkatkan potensi-potensi tersebut dibutuhkan kerjasama, baik dari masyarakat setempat, pemerintah daerah,
+    dan pemerintah pusat untuk bisa terus menerus mengembangkan infrastruktur setiap daerah agar mampu menarik perhatian para investor untuk menanamkan
+    modalnya.
+    </div>
+    """, unsafe_allow_html=True)
+
+with tab4:
+    st.header('Sumber Data')
+    st.markdown("""
+    - https://opendata.jabarprov.go.id/id/dataset/jumlah-proyek-penanaman-modal-asing-berdasarkan-kabupatenkota-di-jawa-barat
+    - https://opendata.jabarprov.go.id/id/dataset/jumlah-proyek-penanaman-modal-dalam-negeri-berdasarkan-kabupatenkota-di-jawa-barat
+    - https://opendata.jabarprov.go.id/id/dataset/jumlah-realisasi-investasi-penanaman-modal-dalam-negeri-berdasarkan-kabupatenkota-di-jawa-barat
+    - https://opendata.jabarprov.go.id/id/dataset/jumlah-penyerapan-tenaga-kerja-penanaman-modal-asing-berdasarkan-kabupatenkota-di-jawa-barat
+    - https://opendata.jabarprov.go.id/id/dataset/jumlah-penyerapan-tenaga-kerja-penanaman-modal-dalam-negeri-berdasarkan-kabupatenkota-di-jawa-barat
+    - https://opendata.jabarprov.go.id/id/dataset/jumlah-pengangguran-terbuka-berdasarkan-jenis-kelamin-di-jawa-barat
+    - https://opendata.jabarprov.go.id/id/dataset/jumlah-angkatan-kerja-berdasarkan-jenis-kelamin-di-jawa-barat
 """)
